f9d0f75953e57129c9d12550f7f5328042b8ab31
shenleiGOOD <3118281629@qq.com>
2018-12-19 11:22:17 +0800

    配置 rabbitmq 文件

--- feign/src/main/java/com/jk/controller/OrderController.java
+++ feign/src/main/java/com/jk/controller/OrderController.java
@@ -1,14 +1,19 @@
 package com.jk.controller;
 
 
+import com.alibaba.fastjson.JSON;
 import com.jk.model.Order;
 import com.jk.service.OrderService;
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 
 
 @Controller
@@ -24,5 +29,15 @@ public class OrderController {
         return orderService.queryOrderList();
         //aaaaaa
     }
-
+    @Autowired
+    private AmqpTemplate amqpTemplate;
+    @RequestMapping("saveBook")
+    @ResponseBody
+    public void insertBook(){
+        //convertAndSend 向队列发送一个消息 第一个参数为队列名称 第二个参数为队列内容
+        Map<String, Object> bookMap = new HashMap<>();
+        bookMap.put("bookId","a56sds4fd5gf");
+        bookMap.put("bookName","三国演义");
+        amqpTemplate.convertAndSend("myqueue", JSON.toJSONString(bookMap));
+    }
 }
--- provider/src/main/java/com/jk/controllerImpl/OrderServiceImpl.java
+++ provider/src/main/java/com/jk/controllerImpl/OrderServiceImpl.java
@@ -3,10 +3,12 @@ package com.jk.controllerImpl;
 import com.jk.mapper.OrderMapper;
 import com.jk.model.Order;
 import com.jk.service.OrderServiceApi;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.alibaba.fastjson.JSON;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class OrderServiceImpl implements OrderServiceApi {
@@ -17,5 +19,10 @@ public class OrderServiceImpl implements OrderServiceApi {
     @Override
     public List<Order> queryOrderList() {
         return orderMapper.queryOrderList();
+    }
+    @RabbitListener(queues = "myqueue")
+    public void listenerBookMessage(String message){
+        Map map = JSON.parseObject(message, Map.class);
+        System.out.println("message = [" + map.toString() + "]");
     }
 }
