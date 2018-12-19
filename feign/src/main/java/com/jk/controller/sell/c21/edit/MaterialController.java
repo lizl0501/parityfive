58742e802602c659ecb8bbc4027fbcaa98d3ade4
lzl <luran927@126.com>
2018-12-19 21:30:50 +0800

    添加 Mapper 分层

--- feign/src/main/java/com/jk/controller/sell/c21/edit/MaterialController.java
+++
@@ -1,9 +0,0 @@
-package com.jk.controller.sell.c21.edit;
-
-import org.springframework.stereotype.Controller;
-
-@Controller("material")
-public class MaterialController {
-
-
-}
---
+++ feign/src/main/java/com/jk/service/sell/testsell.java
@@ -0,0 +1,4 @@
+package com.jk.service.sell;
+
+public class testsell {
+}
